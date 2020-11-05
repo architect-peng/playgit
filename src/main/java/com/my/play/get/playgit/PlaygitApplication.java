e289b445665f9c38b65130e0265d7561452a60d0
15718852195@163.com <6552393peng>
2020-11-05 17:34:04 +0800

    项目初始化

---
+++ src/main/java/com/my/play/get/playgit/PlaygitApplication.java
@@ -0,0 +1,13 @@
+package com.my.play.get.playgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaygitApplication.class, args);
+    }
+
+}
